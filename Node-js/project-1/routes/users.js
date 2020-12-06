d2535997708572af407743fbaed02d59a2c97509
yusuf <ysfozturk3@gmail.com>
2020-12-06 09:54:44 +0100

    created controller

--- Node-js/project-1/routes/users.js
+++ Node-js/project-1/routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers/usersController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.get_users);
 
 module.exports = router;
