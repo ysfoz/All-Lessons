92b0cfb100a0f774e21d020b86e3c0ae7f54e34c
yusuf <ysfozturk3@gmail.com>
2020-12-16 12:57:31 +0100

    added clock

--- Node-js/mern-project/routes/router.js
+++ Node-js/mern-project/routes/router.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const AuthRouter = require("./AuthRouter");
 const ProfileRouter = require("./ProfileRouter");
-// const BookRouter = require("./BookRouter");
+const BookRouter = require("./BookRouter");
 
 // Only /api endpoint
 
@@ -23,6 +23,6 @@ router.use("/profile", ProfileRouter);
  * @route /api/book
  * @desc Route for Book
  */
-// router.use("/books", BookRouter);
+router.use("/books", BookRouter);
 
 module.exports = router;
